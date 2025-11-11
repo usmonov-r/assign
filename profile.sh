6431b24e21a86165a124fbfe013870a297316e2f
usmonov-r <usmonovrakhmat@gmail.com>
2025-11-11 13:00:22 +0900

    sds

---
+++ profile.sh
@@ -0,0 +1,7 @@
+
+if [ $# -eq 2 ]
+then
+  echo "Name: $1, Student ID: $2"
+else
+  echo "Syntax Error : $0 <name> <student_id>"
+fi
---
+++ sample.sh
@@ -0,0 +1,7 @@
+
+if [ $# -eq 4 ]
+then
+  echo "command $0 and arguments are $1, $2, $3, $4"
+else
+  echo "Syntax Error : $0 arg1 arg2 arg3 arg4"
+fi
